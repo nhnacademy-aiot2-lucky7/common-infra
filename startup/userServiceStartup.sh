8e930d28bd3b139d216544a57e1fb8fd16fd72ca
st120712 <zzw123@naver.com>
2025-04-29 19:39:11 +0900

    .

--- startup/userServiceStartup.sh
+++ startup/userServiceStartup.sh
@@ -1,5 +1,5 @@
 pkill -f 'UserService-0.0.1-SNAPSHOT.jar'
-/usr/local/java/java21/bin/java -Dspring.profiles.active=releas -jar ~/target/UserService-0.0.1-SNAPSHOT.jar > ~/log 2>&1 &
+/usr/local/java/java21/bin/java -Dspring.profiles.active=release -jar ~/target/UserService-0.0.1-SNAPSHOT.jar > ~/log 2>&1 &
 
 #권한 755 설정
 chmod 755 ~/startup/userServiceStartup.sh
