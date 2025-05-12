076c6ad0d566f29f5f0e8a2b722a0ac537c9a32e
st120712 <zzw123@naver.com>
2025-05-12 20:02:43 +0900

    Changes

--- startup/serverResourceServiceStartup.sh
+++
@@ -1,5 +0,0 @@
-pkill -f 'server-resource-service-0.0.1-SNAPSHOT.jar'
-/usr/local/java/java21/bin/java -Dserver.port=10244 -jar ~/target/server-resource-service-0.0.1-SNAPSHOT.jar > ~/log 2>&1 &
-
-#권한 755 설정
-chmod 755 ~/startup/serverResourceServiceStartup.sh
